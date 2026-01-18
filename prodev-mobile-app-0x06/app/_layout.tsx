04283f03a971ff6956fc13975deb4f8b44460d3d
juskins <omojuwababatunde1@gmail.com>
2026-01-18 21:43:34 +0100

    Setup nativewind

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -1,10 +1,8 @@
 import { Stack } from "expo-router";
-import { View } from 'react-native'
 
 export default function RootLayout() {
   return (
     <Stack screenOptions={{
-      headerShown: false,
     }}>
       <Stack.Screen name="/" />
       <Stack.Screen name="join" />
